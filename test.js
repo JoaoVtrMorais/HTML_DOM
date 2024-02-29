173e981c35c65f75a900b3d5db33c4ff0e8a969a
João Vitor <103777061+JoaoVtrMorais@users.noreply.github.com>
2024-02-29 19:02:08 -0300

    Atividade - 29/02
    1 - Colocar apenas a mensagem (cor do texto) "Valor inválido" na cor vermelha;
    2 - Programar a validação do Desconto;
    3 - Colocar a linha toda na cor vermelha quando o Valor Unitário estiver errado.

--- test.js
+++ test.js
@@ -1,7 +1,33 @@
-const lista = document.querySelectorAll(".info-total");
-const lista2 = document.querySelectorAll(".info-valor");
-const lista3 = document.querySelectorAll(".info-desconto");
+var usuarios = document.querySelectorAll(".Usuario");
 
-for (var i = 0; i < lista.length; i++) {
-    lista[i].textContent = lista2[i].textContent - ((lista2[i].textContent * lista3[i].textContent) / 100);
+for (var i = 0; i < usuarios.length; i++) {
+    
+    valor = usuarios[i].querySelector(".info-valor").textContent
+    
+    desconto = usuarios[i].querySelector(".info-desconto").textContent
+
+    // Valida o valor do desconto
+    if (desconto < 0 || isNaN(desconto)) {
+        // Valor NOK, avisa o usuário
+        usuarios[i].querySelector(".info-desconto").textContent = "Valor inválido!";
+        usuarios[i].querySelector(".info-desconto").style.color = "red";
+        // usuarios[i].querySelector(".info-total").style.backgroundColor = "red";
+    } else {
+        // Valor OK, prossegue
+        // Calcula o valor total do jogo com desconto aplicado
+        usuarios[i].querySelector(".info-total").textContent = calculaValorComDesconto(valor, desconto);
+    }
+    
+    // Valida o valor total
+    if (valor > 250 || valor < 0 || isNaN(valor)) {
+        usuarios[i].style.color = "red";
+    }
+
+}
+// Função para calcular o valor total com desconto
+
+function calculaValorComDesconto(valor, desconto) {
+    var total = 0;
+    total = valor - ((valor * desconto) / 100);
+    return total;
 }
