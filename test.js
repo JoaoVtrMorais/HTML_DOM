da90d82043114a50cc2dd966e7de3af6d21774f3
João Vitor <103777061+JoaoVtrMorais@users.noreply.github.com>
2024-03-07 20:12:46 -0300

    Alterando a lógica do test.js

--- test.js
+++ test.js
@@ -2,58 +2,52 @@ var usuarios = document.querySelectorAll(".Usuario");
 
 for (var i = 0; i < usuarios.length; i++) {
 
-    valor = usuarios[i].querySelector(".info-valor").textContent
-    
-    if (typeof valor != "number"){
-        valor = parseFloat(valor)
-    }
+    valor = parseFloat(usuarios[i].querySelector(".info-valor").textContent)
+    desconto = parseFloat(usuarios[i].querySelector(".info-desconto").textContent)
+    total = calculaValorComDesconto(valor, desconto)
 
-    usuarios[i].querySelector(".info-total").textContent = valor
-
-    desconto = usuarios[i].querySelector(".info-desconto").textContent
-
-    total = calculaValorComDesconto(valor, desconto);
+    usuarios[i].querySelector(".info-valor").textContent = formataEmValorMonetário(valor)
+    usuarios[i].querySelector(".info-desconto").textContent = formataEmPorcentagem(desconto)
 
     // Valida o valor do desconto
     if (desconto < 0 || isNaN(desconto)) {
         // Valor NOK, avisa o usuário
-        usuarios[i].querySelector(".info-desconto").textContent = "Valor inválido!";
-        usuarios[i].querySelector(".info-desconto").style.color = "red";
+        usuarios[i].querySelector(".info-desconto").textContent = "Valor inválido!"
+        usuarios[i].querySelector(".info-desconto").style.color = "red"
         // usuarios[i].querySelector(".info-total").style.backgroundColor = "red";
     } else {
         // Valor OK, prossegue
         // Calcula o valor total do jogo com desconto aplicado
-        usuarios[i].querySelector(".info-total").textContent = total;
+        usuarios[i].querySelector(".info-total").textContent = formataEmValorMonetário(total)
     }
     
     // Valida o valor 
     if (valor <= 0 || isNaN(valor)) {
-        usuarios[i].style.color = "red";
+        usuarios[i].style.color = "red"
         usuarios[i].querySelector(".info-valor").textContent = "Valor inválido!"
-        usuarios[i].querySelector(".info-total").textContent = 0;
-        usuarios[i].querySelector(".info-desconto").textContent = 0;
+        usuarios[i].querySelector(".info-total").textContent = 0
+        usuarios[i].querySelector(".info-desconto").textContent = 0
     }
 }
 // Função para calcular o valor total com desconto
 
 function calculaValorComDesconto(valor, desconto) {
-    
-    if (typeof valor != "number") {
-        parseFloat(valor)
-    }
-    
-    var total = 0;
-    total = valor - ((valor * desconto) / 100);
-    return formataEmValorMonetário(total);
+    valorFloat = parseFloat(valor)
+    descontoFloat = parseFloat(desconto)
+    var total = 0
+    total = valor - ((valor * desconto) / 100)
+    return total
 }
 
 function formataEmValorMonetário(valor) {
-    valorFloat = parseFloat(valor)
-    valorMonetario = valorFloat.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'});
-    return valorMonetario
+    if (typeof valor == "number") {    
+        valorMonetario = valor.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'});
+        return valorMonetario
+    } else {
+        return "Valor inválido!"
+    }
 }
 
 function formataEmPorcentagem(valor) {
-    porcentagem = valor.toLocaleString()
-    return porcentagem
+    return `${valor}%`
 }
